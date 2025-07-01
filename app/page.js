38fca95bf3b185fd085db3d7c31c1b67be6b4d1b
bnfhayyob <boyabenfath@gmail.com>
2025-07-02 01:49:54 +0200

    Add my list of Blog stored in my assets

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client'
-import Header from "@/Components/Header";
+import Header from "@/Components/Header"
 import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <BlogItem/>
+      <BlogList/>
     </>
   );
 }
