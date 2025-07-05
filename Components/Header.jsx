95b769356a75b484b982af102ecedd4fdd18d995
bnfhayyob <boyabenfath@gmail.com>
2025-07-05 14:10:22 +0200

    adding the email subscription part

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,25 @@
 import { assets } from '@/Assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const Header = () => {
+
+  const [email,setEmail] = useState("")
+  const onSubmitHandler = async (e) =>{
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append('email',email)
+    const response = await axios.post('/api/email',formData)
+     if (response.data.success || response.data.status === 'success' || response.status === 200){
+      toast.success(response.data.msg || response.data.message || "Success!")
+      setEmail("")
+    }else{
+       toast.error(response.data.msg || response.data.message || "Error")
+    }
+  }
+
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
         <div className='flex justify-between items-center'>
@@ -12,8 +29,8 @@ const Header = () => {
         <div className='text-center my-8'>
             <h1 className='text-3xl sm:text-5xl font-mendium'>Laest Blogs</h1>
             <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus porro recusandae omnis delectus animi perferendis dolores illum amet quod, nostrum voluptatem, officiis repudiandae quas rerum natus velit, numquam eaque saepe!</p>
-            <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
-                <input type="email" placeholder='Enter Your Email' className='pl-4 outline-none'/>
+            <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]' action="">
+                <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Enter Your Email' className='pl-4 outline-none'/>
                 <button type='submt' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
             </form>
         </div>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client'
 import Header from "@/Components/Header"
-import BlogItem from "@/Components/BlogItem";
 import BlogList from "@/Components/BlogList";
 import Footer from "@/Components/Footer";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <>
+      <ToastContainer theme="dark"/>
       <Header/>
       <BlogList/>
       <Footer/>
